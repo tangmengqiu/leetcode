52d680a5bd4d12a03ae3239d52d167d1ae8f2903
唐孟秋 <tangmengqiu@Diego.local>
2019-06-09 13:04:28 +0800

    add 27.remove-element and 28.implement-str-str

---
+++ src/28.implement-str-str.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (31.50%)
+ * Likes:    887
+ * Dislikes: 1309
+ * Total Accepted:    433.3K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ */
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty()){
+            return 0;
+        }
+        if(needle.size()>haystack.size()){
+            return -1;
+        }
+        unsigned int loc=haystack.find(needle,0);
+        if(loc!=string::npos){
+            return loc;
+        }else{
+            return -1;
+        }
+    }
+};
+
