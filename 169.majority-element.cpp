910414bd9e5835f626067d781a7df8e426a2f590
tangmengqiu <1124090103@qq.com>
2020-02-04 00:06:57 +0800

    solution:169.majority-element

---
+++ 169.majority-element.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=169 lang=cpp
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1) return nums[0];
+        for(int i=0;i<nums.size()-1;i++){
+            int count=0;
+            while(i<nums.size()-1&&nums[i+1]==nums[i]){
+                count++;
+                i++;
+            }
+            if(count>=nums.size()/2)
+                return nums[i];
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
