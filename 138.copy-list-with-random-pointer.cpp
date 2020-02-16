43c802998f9d5268a0b25106c44bf15d66fcb894
tangmengqiu <1124090103@qq.com>
2020-02-16 22:58:56 +0800

    138.copy-list-with-random-pointer

---
+++ 138.copy-list-with-random-pointer.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=138 lang=cpp
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node *newHead, *l1, *l2;
+        if (head == NULL) return NULL;
+        for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+            l2 = new Node(l1->val);
+            l2->next = l1->next;
+            l1->next = l2;
+        }
+            
+        newHead = head->next;
+        for (l1 = head; l1 != NULL; l1 = l1->next->next) {
+            if (l1->random != NULL) l1->next->random = l1->random->next;
+        }
+            
+        for (l1 = head; l1 != NULL; l1 = l1->next) {
+            l2 = l1->next;
+            l1->next = l2->next;
+            if (l2->next != NULL) l2->next = l2->next->next;
+        }
+
+        return newHead;
+    }
+};
+// @lc code=end
+
